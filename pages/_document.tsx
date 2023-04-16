089842b2d0f4d820b275f6e2967db27978b463c6
Daniel Ladrón de Guevara <daniel.ldg@icloud.com>
2023-04-16 12:11:02 -0600

    configurar _app y _document con mantine

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { ServerStyles, createStylesServer } from "@mantine/next";
+import Document, { DocumentContext } from "next/document";
+import { rtlCache } from "../rtl-cache";
+
+const stylesServer = createStylesServer(rtlCache);
+
+export default class _Document extends Document {
+	static async getInitialProps(ctx: DocumentContext) {
+		const initialProps = await Document.getInitialProps(ctx);
+
+		return {
+			...initialProps,
+			styles: [initialProps.styles, <ServerStyles html={initialProps.html} server={stylesServer} key='styles' />],
+		};
+	}
+}
