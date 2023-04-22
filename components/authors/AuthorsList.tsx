7d2815d1aab0d21b801a30073da40af7f48b624c
Daniel Ladrón de Guevara <daniel.ldg@icloud.com>
2023-04-22 01:14:15 -0600

    ajustes en cards
    no wrap y se oculta ubicación en celular

--- components/authors/AuthorsList.tsx
+++ components/authors/AuthorsList.tsx
@@ -1,4 +1,5 @@
-import { Box, Card, Flex, Group, Text } from "@mantine/core";
+import useViewportWidth from "@/hooks/useViewportWidth";
+import { Box, Card, Group, Stack, Text } from "@mantine/core";
 import { Author } from "@prisma/client";
 import { IconMapPin, IconSchool } from "@tabler/icons-react";
 import Image from "next/image";
@@ -13,39 +14,38 @@ interface IProps {
 }
 
 const AuthorsList: React.FC<IProps> = ({ authors }) => {
+	const { sm } = useViewportWidth();
 	return (
 		<Box w='100%'>
 			{authors.map((author, i) => (
 				<Link key={i} href={`/autor/${author.url}`} passHref legacyBehavior>
 					<Card withBorder component='a' mb='lg'>
-						<Group>
-							<Box>
-								<Image src={author.picture} alt={`Fotografía de ${author.name}`} width={60} height={60} />
-							</Box>
-							<Box>
-								<Flex justify='flex-start' align='flex-start' direction='column'>
-									<Group>
+						<Group noWrap>
+							<Image src={author.picture} alt={`Fotografía de ${author.name}`} width={60} height={60} />
+							<Stack spacing={0}>
+								<Group>
+									{sm && (
 										<Group spacing={5}>
 											<IconMapPin size={18} color='#868e96' />
 											<Text size='sm' c='dimmed'>
 												{author.location}
 											</Text>
 										</Group>
-										<Group spacing={5}>
-											<IconSchool size={18} color='#868e96' />
-											<Text size='sm' c='dimmed'>
-												{author.degree}
-											</Text>
-										</Group>
-									</Group>
-									<Text fz='xl' fw={500}>
-										{author.name}
-									</Text>
-									{author._count?.posts !== undefined && (
-										<Text fs='italic' fz='sm' c='dimmed'>{`${author._count.posts} Posts`}</Text>
 									)}
-								</Flex>
-							</Box>
+									<Group spacing={5}>
+										<IconSchool size={18} color='#868e96' />
+										<Text size='sm' c='dimmed'>
+											{author.degree}
+										</Text>
+									</Group>
+								</Group>
+								<Text fz='xl' fw={500}>
+									{author.name}
+								</Text>
+								{author._count?.posts !== undefined && (
+									<Text fs='italic' fz='sm' c='dimmed'>{`${author._count.posts} Posts`}</Text>
+								)}
+							</Stack>
 						</Group>
 					</Card>
 				</Link>
