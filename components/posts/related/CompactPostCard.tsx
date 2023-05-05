6cdfc166a7ca5f11df509c9dfdb5f3dc774dd1c6
Daniel Ladrón de Guevara <daniel.ldg@icloud.com>
2023-05-04 23:59:12 -0600

    separar CompactPostCard en otro archivo

---
+++ components/posts/related/CompactPostCard.tsx
@@ -0,0 +1,62 @@
+import { SimilarPostsResponse } from "@/pages/api/getSimilar";
+import { Flatten } from "@/utils/TypeUtils";
+import { Card, Stack, Text, Title, createStyles } from "@mantine/core";
+import Image from "next/image";
+import { LinkProps } from "next/link";
+import React from "react";
+
+const useStyles = createStyles(theme => ({
+	backgroudImage: {
+		zIndex: 0,
+		objectFit: "cover",
+		objectPosition: "center",
+	},
+	overlay: {
+		position: "absolute",
+		top: 0,
+		left: 0,
+		right: 0,
+		bottom: 0,
+		backgroundImage: "linear-gradient(180deg, rgba(0, 0, 0, .2) 0%, rgba(0, 0, 0, .9) 90%)",
+		zIndex: 1,
+	},
+	content: {
+		position: "relative",
+		height: "100%",
+		zIndex: 1,
+	},
+}));
+
+interface IPropsCard extends Partial<LinkProps> {
+	post: Flatten<SimilarPostsResponse>;
+}
+
+const CompactPostCard = React.forwardRef<HTMLAnchorElement, IPropsCard>(({ href, onClick, post }, ref) => {
+	const { classes } = useStyles();
+
+	return (
+		<Card
+			component='a'
+			href={href?.toString()}
+			onClick={onClick}
+			ref={ref}
+			withBorder
+			h={{ base: 100, sm: 200 }}
+			radius='md'>
+			<Image className={classes.backgroudImage} src={post.image.url} alt={post.image.alt} fill />
+			<div className={classes.overlay} />
+			<Stack className={classes.content} align='flex-start' justify='flex-end' spacing={0}>
+				<Title order={5} c={"#FFF"}>
+					{post.title}
+				</Title>
+				<Text c={"#FFF"} fz='xs' fs='italic' fw={500}>
+					{post.author.name}
+				</Text>
+			</Stack>
+		</Card>
+	);
+});
+
+CompactPostCard.displayName = "CompactPostCard";
+
+export default CompactPostCard;
--- components/posts/related/RelatedPosts.tsx
+++ components/posts/related/RelatedPosts.tsx
@@ -1,8 +1,8 @@
 import { SimilarPostsResponse } from "@/pages/api/getSimilar";
-import { Card, Grid, Group, Stack, Text, Title, createStyles } from "@mantine/core";
-import Image from "next/image";
-import Link, { LinkProps } from "next/link";
+import { Grid, Group, Title } from "@mantine/core";
+import Link from "next/link";
 import React, { useEffect, useState } from "react";
+import CompactPostCard from "./CompactPostCard";
 
 interface IProps {
 	postUrl: string;
@@ -27,7 +27,7 @@ const RelatedPosts: React.FC<IProps> = ({ postUrl }) => {
 				{posts.map((post, i) => (
 					<Grid.Col xs={12} sm={4} key={i}>
 						<Link passHref legacyBehavior href={`/post/${post.url}`}>
-							<PostCard post={post} />
+							<CompactPostCard post={post} />
 						</Link>
 					</Grid.Col>
 				))}
@@ -36,59 +36,5 @@ const RelatedPosts: React.FC<IProps> = ({ postUrl }) => {
 		</section>
 	);
 };
-
-const useStyles = createStyles(theme => ({
-	backgroudImage: {
-		zIndex: 0,
-		objectFit: "cover",
-		objectPosition: "center",
-	},
-	overlay: {
-		position: "absolute",
-		top: 0,
-		left: 0,
-		right: 0,
-		bottom: 0,
-		backgroundImage: "linear-gradient(180deg, rgba(0, 0, 0, .2) 0%, rgba(0, 0, 0, .9) 90%)",
-		zIndex: 1,
-	},
-	content: {
-		position: "relative",
-		height: "100%",
-		zIndex: 1,
-	},
-}));
-
-interface IPropsCard extends Partial<LinkProps> {
-	post: Flatten<SimilarPostsResponse>;
-}
-
-const PostCard = React.forwardRef<HTMLAnchorElement, IPropsCard>(({ href, onClick, post }, ref) => {
-	const { classes } = useStyles();
-
-	return (
-		<Card
-			component='a'
-			href={href?.toString()}
-			onClick={onClick}
-			ref={ref}
-			withBorder
-			h={{ base: 100, sm: 200 }}
-			radius='md'>
-			<Image className={classes.backgroudImage} src={post.image.url} alt={post.image.alt} fill />
-			<div className={classes.overlay} />
-			<Stack className={classes.content} align='flex-start' justify='flex-end' spacing={0}>
-				<Title order={5} c={"#FFF"}>
-					{post.title}
-				</Title>
-				<Text c={"#FFF"} fz='xs' fs='italic' fw={500}>
-					{post.author.name}
-				</Text>
-			</Stack>
-		</Card>
-	);
-});
-
-type Flatten<Type> = Type extends Array<infer Item> ? Item : Type;
 
 export default RelatedPosts;
