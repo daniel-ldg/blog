914d6b1aa51fc51375c49f634b9713fd05f7a1ec
Daniel Ladrón de Guevara <daniel.ldg@icloud.com>
2023-05-09 00:10:51 -0600

    actualizar con opcion de filtrar con keyword

--- hooks/usePostFetch.ts
+++ hooks/usePostFetch.ts
@@ -11,18 +11,20 @@ interface IFetcherArgs {
 	author?: string;
 	skip?: number;
 	take?: number;
+	keyword?: string;
 }
 
-const fetcher: SWRInfiniteFetcher<ExtendedPost[], SWRInfiniteKeyLoader<ExtendedPost[], IFetcherArgs | null>> = args => {
+const fetcher: SWRInfiniteFetcher<ExtendedPost[], SWRInfiniteKeyLoader<ExtendedPost[], IFetcherArgs | null>> = async args => {
 	const validation = z
 		.object({
 			author: z.string().optional(),
 			skip: z.undefined().or(z.number().transform(n => `${n}`)),
 			take: z.undefined().or(z.number().transform(n => `${n}`)),
+			keyword: z.string().optional(),
 		})
 		.safeParse(args);
 	if (!validation.success) {
-		return [];
+		throw new Error("wrong args");
 	}
 	let { data: params } = validation;
 	params = Object.fromEntries(Object.entries(params).filter(([_, v]) => v != undefined));
@@ -34,19 +36,19 @@ const fetcher: SWRInfiniteFetcher<ExtendedPost[], SWRInfiniteKeyLoader<ExtendedP
 		.then(text => SuperJSON.parse(text));
 };
 
-const getKeyFn = (take: number, author?: string) => {
+const getKeyFn = (take: number, filter?: { authorUrl?: string; keyword?: string }) => {
 	const getKey: SWRInfiniteKeyLoader<ExtendedPost[], IFetcherArgs | null> = (index, previousPageData) => {
 		if (previousPageData && previousPageData.length < take) {
 			return null;
 		}
-		return { author, take, skip: index * take };
+		return { take, skip: index * take, author: filter?.authorUrl, keyword: filter?.keyword };
 	};
 	return getKey;
 };
 
-export const useSWRInfinitePosts = (take: number = DEFAULT_TAKE, author?: string) => {
+export const useSWRInfinitePosts = (take: number = DEFAULT_TAKE, filter?: { authorUrl: string } | { keyword: string }) => {
 	const [isEndReached, setIsEndReached] = useState(false);
-	const { data, setSize, ...swrInfinite } = useSWRInfinite(getKeyFn(take, author), fetcher);
+	const { data, setSize, ...swrInfinite } = useSWRInfinite(getKeyFn(take, filter), fetcher);
 
 	useEffect(() => {
 		setIsEndReached((data?.at(-1)?.length || -Infinity) < take);
