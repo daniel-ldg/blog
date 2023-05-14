cf6cb3aaa2afd6377da6f7f8dff120331c9745eb
Daniel Ladrón de Guevara <daniel.ldg@icloud.com>
2023-05-13 21:44:43 -0600

    bugfix logo wrong version on page refresh

--- components/header/Logo.tsx
+++ components/header/Logo.tsx
@@ -5,11 +5,12 @@ import Link from "next/link";
 const Logo = () => {
 	const { colorScheme } = useMantineColorScheme();
 	const isDark = colorScheme === "dark";
+	const logo = isDark ? "/scriniun_logo_light.svg" : "/scriniun_logo.svg";
 
 	return (
 		<Link href={"/"} passHref legacyBehavior>
 			<Box component='a'>
-				<Image src={isDark ? "/scriniun_logo_light.svg" : "/scriniun_logo.svg"} alt='scriniun' height={18} width={85} />
+				<Image key={`${isDark}`} src={logo} alt='scriniun logo' height={18} width={85} />
 			</Box>
 		</Link>
 	);
